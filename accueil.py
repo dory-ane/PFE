bf8a6372cfb3d2149adec7c38e853c78c6f0a21e
dory-ane <pacyguimatio@gmail.com>
2023-08-27 17:42:17 +0200

    Message commit

--- accueil.py
+++ accueil.py
@@ -5,10 +5,10 @@ import seaborn as sns
 
 st.title('Page d\'accueil')
 
-st.markdown("Hello, Bienvenue sur notre application de nettoyage et de visualisation de la donnée"
-            "Merci d'avoir choisi notre étude pour l'analyse de vos données"
-            "En espérant que notre application vous plaira et qu\'elle répondra à vos besoins"
-            "Nous vous souhaitons une agréable expérience et une bonne analyse")
+st.markdown("Hello, Bienvenue sur notre application de nettoyage et de visualisation de la donnée.  "
+            "Merci d'avoir choisi notre solution pour l'analyse de vos données.  "
+            "En espérant que notre application vous plaira et qu\'elle répondra à vos besoins,"
+            "nous vous souhaitons une agréable expérience et une bonne analyse")
 
 st.sidebar.write(
     "<div style='text-align:right; font-size:small;'>Conçu par Doriane Mbouzang </div>",
